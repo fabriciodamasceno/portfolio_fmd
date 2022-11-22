a417b7cebfb45fa4871f38031c7607476fd8181e
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2022-11-22 17:04:04 -0300

    Alterando texto Description

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -2,7 +2,7 @@ export function Description() {
     return(
         <article className="text">
             <h2>Sobre mim</h2>
-            <p>lorem</p>
+            <p>Sou um Desenvolvedor apaixonado por tecnologia e inovações. Estou sempre em busca de conhecimento e acabo contagiando as pessoas ao meu redor com a minha sede de conhecimento incentivando as pessoas a buscarem estar sempre atualizadas com as novidades do mundo Tecnológico. Sou autodidata e estou sempre sobre buscando conhecimento na área de DevOps e Full Stack. Gosto de passar o que sei para as outras pessoas e participo em Comunidades de Desenvolvimento por meio de Meetup, Telegram. Sou organizado, responsável com projeto e prazos e gosto de trabalhar em equipe. Estou sempre buscando aplicar metodologia Ágil em todas as empresas ou lugares que eu trabalho.</p>
         </article>
     )
 }
